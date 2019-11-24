e3488670cd57ff2d621f4acb2331e0047ac9de07
Steven Frasica <sfrasica1@gmail.com>
2019-11-24 02:26:19 +0000

    Automatically backed up by Learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,5 +1,5 @@
 require_relative "../lib/greeting.rb"
 # code the #greeting method here!
 def greeting(name)
-    put "Hello #{name.strip}. It's nice to meet you"
+    put "Hello #{name}. It's nice to meet you."
 end
